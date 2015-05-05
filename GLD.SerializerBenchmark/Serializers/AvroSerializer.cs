fb6e78b52cc22aa4f6d0324712b5c1ab63ff88c4
Jeremy Blosser <jeremy.blosser@projekt202.com>
2015-05-05 17:40:57 -0500

    Improvement to the Avro serializer (keep the serializer around vs. the built-in caching mechanism)

--- GLD.SerializerBenchmark/Serializers/AvroSerializer.cs
+++ GLD.SerializerBenchmark/Serializers/AvroSerializer.cs
@@ -11,25 +11,19 @@ namespace GLD.SerializerBenchmark
 {
     internal class AvroSerializer : ISerDeser
     {
-        private AvroSerializerSettings _settings = new AvroSerializerSettings {UseCache = true};
-        //private static Microsoft.Hadoop.Avro.AvroSerializer _serializer = Microsoft.Hadoop.Avro.AvroSerializer.Create<Person>();;
+        private readonly IAvroSerializer<Person> _serializer = Microsoft.Hadoop.Avro.AvroSerializer.Create<Person>();
 
         #region ISerDeser Members
 
         public AvroSerializer(Type type)
         {
-            // TODO: Hack! How to get a type of the person object? In XmlSerializer it works, not here!
-            // The serialize is typed and type should be know upfront. 
-            // The staic variable cannot be created.
-            //_serializer =  Microsoft.Hadoop.Avro.AvroSerializer.Create<Person>();
         }
 
         public string Serialize<T>(object person)
         {
-            var serializer =  Microsoft.Hadoop.Avro.AvroSerializer.Create<T>(_settings); 
             using (var ms = new MemoryStream())
             {
-                serializer.Serialize(ms, (T)person); 
+                _serializer.Serialize(ms, (Person)person); 
                 ms.Flush();
                 ms.Position = 0;
                 return Convert.ToBase64String(ms.ToArray());
@@ -38,12 +32,11 @@ namespace GLD.SerializerBenchmark
 
         public T Deserialize<T>(string serialized)
         {
-            var serializer =  Microsoft.Hadoop.Avro.AvroSerializer.Create<T>(_settings);
             var b = Convert.FromBase64String(serialized);
             using (var stream = new MemoryStream(b))
             {
                 stream.Seek(0, SeekOrigin.Begin);
-                return (T)serializer.Deserialize(stream); 
+                return (T) ((object) _serializer.Deserialize(stream));
             }
         }
 
