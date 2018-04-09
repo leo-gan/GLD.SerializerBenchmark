fcc82300ac32a69d1531ccf0ea6dab699cebb44c
leo_g <leo.gan.57@gmail.com>
2018-04-09 10:57:41 -0700

    added the results and the result explanations files

--- GLD.SerializerBenchmark/Program.cs
+++ GLD.SerializerBenchmark/Program.cs
@@ -16,7 +16,7 @@ namespace GLD.SerializerBenchmark
                 new IntDescription(),
                 new StringArrayDescription(),
                 new SimpleObjectDescription(),
-                //new TelemetryDescription(),
+                new TelemetryDescription(),
                 //new EDI_X12_835Description(),
                 //new TestData.NoAtributes.EDI_X12_835Description(),
                 //new ObjectGraphDescription(), // TODO: Exception. Investigate.
@@ -24,37 +24,36 @@ namespace GLD.SerializerBenchmark
             };
             var serializers = new List<ISerDeser>
             {
-                //// new AvroSerializer(),  //TODO: For some reason it is impossible to pass generic T type to serializer. 
+                // new AvroSerializer(),  //TODO: For some reason it is impossible to pass generic T type to serializer. 
                 new BinarySerializer(),
-                //new BondCompactSerializer(), // TODO: works only for Person
-                //new BondFastSerializer(), // TODO: works only for Person
-                //new BondJsonSerializer(), // TODO: works only for Person
-                //new DataContractSerializerSerializer(),
-                //new DataContractJsonSer(),
-                //new JavaScriptSerializer(), // TODO: DateTime format?
-                //new XmlSerializerSer(),
-                ////new ApJsonSerializer(), // TODO: DateTime format?
-                //new FastJsonSerializer(), // TODO: DateTime format?
-                //new JilSerializer(), // TODO: DateTime format?
-                //new JsonFxSerializer(),
-                //// TODO: DateTime format? // TODO !!! on ObjectGraphDescription gets system exception.
-                //new JsonNetHelperSerializer(),
-                //new JsonNetSerializer(),
-                //new HaveBoxJSONSerializer(), // TODO: DateTime format?
+                new BondCompactSerializer(), // TODO: works only for Person
+                new BondFastSerializer(), // TODO: works only for Person
+                new BondJsonSerializer(), // TODO: works only for Person
+                new DataContractSerializerSerializer(),
+                new DataContractJsonSer(),
+                new JavaScriptSerializer(), // TODO: DateTime format?
+                new XmlSerializerSer(),
+                //new ApJsonSerializer(), // TODO: DateTime format?
+                new FastJsonSerializer(), // TODO: DateTime format?
+                new JilSerializer(), // TODO: DateTime format?
+                new JsonFxSerializer(),
+                new JsonNetHelperSerializer(),
+                new JsonNetSerializer(),
+                new HaveBoxJSONSerializer(), // TODO: DateTime format?
                 new FsPicklerBinarySerializer(),
                 new FsPicklerJsonSerializer(),
-                //new MessageSharkSer(),
-                //new MsgPackSerializer(), // TODO: DateTime format?
-                //new NetJSONSer(), // TODO: DateTime format?
-                //new NetSerializerSer(),
-                //new NfxJsonSerializer(),
-                //new NfxSlimSerializer(),
-                //new ProtoBufSerializer(),
-                //new SharpSerializer(), // TODO: DateTime format?
-                //new ServiceStackJsonSerializer(), // TODO: DateTime format?
-                //new ServiceStackTypeSerializer(), // TODO: DateTime format?
-                //new SalarBoisSerializer(),
-                //new WireSerializer()
+                new MessageSharkSer(),
+                new MsgPackSerializer(), // TODO: DateTime format?
+                new NetJSONSer(), // TODO: DateTime format?
+                new NetSerializerSer(),
+                new NfxJsonSerializer(),
+                new NfxSlimSerializer(),
+                new ProtoBufSerializer(),
+                new SharpSerializer(), // TODO: DateTime format?
+                new ServiceStackJsonSerializer(), // TODO: DateTime format?
+                new ServiceStackTypeSerializer(), // TODO: DateTime format?
+                new SalarBoisSerializer(),
+                new WireSerializer()
             };
 
             Tester.Tests(serializers, testDataDescriptions, repetitions);
